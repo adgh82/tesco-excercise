9758e6a56a696c7e6ae5c9b3a88d8baf4fbd9221
Aditya Ghosh <aghosh4@sapient.com>
2016-12-04 16:04:34 +0530

    Initial Setup

--- tdd-exercise/rock-paper-scissor/src/test/java/org/sape/tesco/model/ModelTest.java
+++ tdd-exercise/rock-paper-scissor/src/test/java/org/sape/tesco/model/ModelTest.java
@@ -1,14 +1,12 @@
 package org.sape.tesco.model;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class ModelTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
 	}
 
 }
