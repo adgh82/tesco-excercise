acffd523309c16d03f3828709cae19857e3d6bf9
Aditya Ghosh <aghosh4@sapient.com>
2016-12-04 20:02:07 +0530

    Refactored main class

--- tdd-exercise/perfect-number/src/main/java/org/sape/tesco/perfectnumber/NumberChecker.java
+++ tdd-exercise/perfect-number/src/main/java/org/sape/tesco/perfectnumber/NumberChecker.java
@@ -6,25 +6,34 @@ public class NumberChecker {
 	
 	public static boolean checkNumber(int numberToBeChecked){
 		boolean result=false;
-		if(numberToBeChecked>1){
-			int halfNumber;
-			if(numberToBeChecked%NUMBER_TWO==0){
-				halfNumber = (int)numberToBeChecked/NUMBER_TWO;
-			}else{
-				halfNumber = (int)(numberToBeChecked/NUMBER_TWO)+1;
-			}
-			int counter;
-			int accumulator = 1;
-			for(counter=NUMBER_TWO;counter<=halfNumber;counter++){
-				if(numberToBeChecked%counter==0){
-					accumulator+=counter;
-				}
-			}
+		if(numberToBeChecked>1){			
+			int accumulator = accumulateDivisor(numberToBeChecked, getHalfNumber(numberToBeChecked));
 			if(numberToBeChecked==accumulator){
 				result=true;
 			}
 		}
 		return result;
+	}
+
+	private static int accumulateDivisor(int numberToBeChecked, int halfNumber) {
+		int counter;
+		int accumulator = 1;
+		for(counter=NUMBER_TWO;counter<=halfNumber;counter++){
+			if(numberToBeChecked%counter==0){
+				accumulator+=counter;
+			}
+		}
+		return accumulator;
+	}
+
+	private static int getHalfNumber(int numberToBeChecked) {
+		int halfNumber;
+		if(numberToBeChecked%NUMBER_TWO==0){
+			halfNumber = (int)numberToBeChecked/NUMBER_TWO;
+		}else{
+			halfNumber = (int)(numberToBeChecked/NUMBER_TWO)+1;
+		}
+		return halfNumber;
 	}
 
 }
